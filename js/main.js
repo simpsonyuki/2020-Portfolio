51a5aad86cc2a8321fb71231222b10ddf2191df5
simpsonyuki <yukilin0327@gmail.com>
2020-07-26 06:41:36 +0800

    back to top

--- js/main.js
+++ js/main.js
@@ -1,2 +1,60 @@
 
   AOS.init();
+
+// back-to-top
+  $(document).ready(function(){
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('#back-to-top').tooltip('show');
+
+});	
+ 
+ $(document).ready(function(){
+
+$(window).scroll(function () {
+
+if ($(this).scrollTop() > 50) {
+
+$('#back-to-top').tooltip();
+
+$('#back-to-top').fadeIn();
+
+} else {
+
+$('#back-to-top').fadeOut();
+
+$('#back-to-top').tooltip('hide');
+
+}
+
+});
+
+// scroll body to 0px on click
+
+$('#back-to-top').click(function () {
+
+$('body,html').animate({
+
+scrollTop: 0
+
+}, 800);
+
+return false;
+
+});
+
+});

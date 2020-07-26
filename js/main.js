ac56de7882cb48d106efa06d571665f22a22fccb
simpsonyuki <yukilin0327@gmail.com>
2020-07-26 18:53:50 +0800

    0726 back-to-top--未完成

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ $(function () {
         $(this).addClass('current');
  
         var selector = $(this).attr('data-filter');
-        console.log(selector);
         $container.isotope({
             filter: selector,
             layoutMode: 'fitRows'
@@ -28,59 +27,3 @@ $(function () {
 
 
 // back-to-top
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-      $('#back-to-top').fadeIn();
-    } else {
-      $('#back-to-top').fadeOut();
-    }
-  });
-  // scroll body to 0px on click
-  $('#back-to-top').click(function () {
-    $('#back-to-top').tooltip('hide');
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
-    return false;
-  });
-
-  $('#back-to-top').tooltip('show');
-
-});
-
-$(document).ready(function () {
-
-  $(window).scroll(function () {
-
-    if ($(this).scrollTop() > 50) {
-
-      $('#back-to-top').tooltip();
-
-      $('#back-to-top').fadeIn();
-
-    } else {
-
-      $('#back-to-top').fadeOut();
-
-      $('#back-to-top').tooltip('hide');
-
-    }
-
-  });
-
-  // scroll body to 0px on click
-
-  $('#back-to-top').click(function () {
-
-    $('body,html').animate({
-
-      scrollTop: 0
-
-    }, 800);
-
-    return false;
-
-  });
-
-});

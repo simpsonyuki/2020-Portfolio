ea2612f3a1d0ea09fb2bd746f6c3d7a9d5ebb7a3
simpsonyuki <yukilin0327@gmail.com>
2020-07-26 16:30:37 +0800

    0726 作品集過濾器

--- js/main.js
+++ js/main.js
@@ -1,70 +1,87 @@
-
-  AOS.init();
+AOS.init();
 
 
 // 漢堡選單
-$(function() {
-    $('.menu').on('click', function() {
-      $(this).toggleClass('active');
-    });
-    
+$(function () {
+  $('.menu').on('click', function () {
+    $(this).toggleClass('active');
   });
+
+});
+
+
+// 作品及篩選器
+ var $container = $('.my-works'),
+      $body = $('body'),
+      colW = 375,
+      columns = null;
+
+  $('.works-nav li a').click(function(){
+        $('.works-nav .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+         });
+         return false;
+    });
 
 
 // back-to-top
-  $(document).ready(function(){
-    $(window).scroll(function () {
-           if ($(this).scrollTop() > 50) {
-               $('#back-to-top').fadeIn();
-           } else {
-               $('#back-to-top').fadeOut();
-           }
-       });
-       // scroll body to 0px on click
-       $('#back-to-top').click(function () {
-           $('#back-to-top').tooltip('hide');
-           $('body,html').animate({
-               scrollTop: 0
-           }, 800);
-           return false;
-       });
-       
-       $('#back-to-top').tooltip('show');
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
 
-});	
- 
- $(document).ready(function(){
-
-$(window).scroll(function () {
-
-if ($(this).scrollTop() > 50) {
-
-$('#back-to-top').tooltip();
-
-$('#back-to-top').fadeIn();
-
-} else {
-
-$('#back-to-top').fadeOut();
-
-$('#back-to-top').tooltip('hide');
-
-}
+  $('#back-to-top').tooltip('show');
 
 });
 
-// scroll body to 0px on click
+$(document).ready(function () {
 
-$('#back-to-top').click(function () {
+  $(window).scroll(function () {
 
-$('body,html').animate({
+    if ($(this).scrollTop() > 50) {
 
-scrollTop: 0
+      $('#back-to-top').tooltip();
 
-}, 800);
+      $('#back-to-top').fadeIn();
 
-return false;
+    } else {
 
-});
+      $('#back-to-top').fadeOut();
+
+      $('#back-to-top').tooltip('hide');
+
+    }
+
+  });
+
+  // scroll body to 0px on click
+
+  $('#back-to-top').click(function () {
+
+    $('body,html').animate({
+
+      scrollTop: 0
+
+    }, 800);
+
+    return false;
+
+  });
 
 });
